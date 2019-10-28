0232d7a4a018cc8ff52a0423b3aec99891ba4292
jeano <33420400+JeanoLee@users.noreply.github.com>
2019-10-28 17:04:13 +0900

    Update DelegateCall.sol

--- DelegateCall.sol
+++ DelegateCall.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.4.22;
+pragma solidity ^0.5.0;
 
 
 contract Callee {
     address public caller;
-    
     event Called(address indexed);
+    event WhoAmI(address indexed);
     
     constructor() public {
     }
@@ -12,10 +12,9 @@ contract Callee {
     function func() public returns (bool result){
         caller = msg.sender;
         emit Called(caller);
+        emit WhoAmI(address(this));
         result = true;
     }
-    
-    
 }
 
 contract Caller {
@@ -24,16 +23,19 @@ contract Caller {
         _callee = Callee(_ca);
     }
     
-    function func1() public returns (bool result){
+    function funcDefaultCall() public returns (bool result){
         result = _callee.func();
     }
     
-    function func2() public returns (bool result){
-        result = address(_callee).call(bytes4(keccak256("func()")));
+    function funcStaticCall() public returns (bool){
+        (bool result,) = address(_callee).call(abi.encodePacked(bytes4(keccak256("func()"))));
+        return result;
+
     }
     
-    function func3() public returns (bool result){
-        result = address(_callee).delegatecall(bytes4(keccak256("func()")));
+    function funcDelegateCall() public returns (bool ){
+        (bool result,) = address(_callee).delegatecall(abi.encodePacked(bytes4(keccak256("func()"))));
+        return result;
     }
     
     
